f5f82a29a84b93f7731c9e54faf1122d9b5dbadc
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-24 22:42:01 +0530

    Refactored: Using 1 generic method find max value logic

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -2,25 +2,27 @@ package com.bridgelabz;
 
 public class Maximum {
 	/*
-	 * This method is used to Find maximum among three Strings value
+	 * This generic method is used to Refactor all the 3(Integer, float, String) to
+	 * One Generic Method and find the maximum value
 	 */
-	public static String maxString(String a, String b, String c) {
-		String max = a;// assume a is initially largest
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
+	public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
+		T max = a; // assume a is initially the largest
 
-		System.out.println("First String= " + a);
-		System.out.println("Sec String= " + b);
-		System.out.println("Third String= " + c);
-		System.out.println("Maximum Value is= " + max);
-		return max;
+		if (b.compareTo(max) > 0) {
+			max = b;
+		}
+		if (c.compareTo(max) > 0) {
+			max = c;
+		}
+		return max; // returns the largest value
 	}
 
 	// main method
 	public static void main(String[] args) {
 		// Calling method with Integer argument
-		Maximum.maxString("Apple", "Peach", "Banana");
+		System.out.printf("Max of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
+		System.out.printf("Max of %.1f,%.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6f, 8.8f, 7.7f));
+		System.out.printf("Max of %s, %s and %s is %s\n", "Apple", "Pear", "Banana",
+				maximum("Apple", "Pear", "Banana"));
 	}
 }
