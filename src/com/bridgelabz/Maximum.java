1e78d2d7750a3102d3c2a0b802762e72a33b389d
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-24 21:38:18 +0530

    Added: Find the max of given 3 float values logic

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -2,10 +2,10 @@ package com.bridgelabz;
 
 public class Maximum {
 	/*
-	 * This method is used to Find maximum number among three integer value
+	 * This method is used to Find maximum number among three Floats value
 	 */
-	public static Integer maxInteger(Integer a, Integer b, Integer c) {
-		Integer max = a;// assume a is initially largest
+	public static Float maxFloat(Float a, Float b, Float c) {
+		Float max = a;// assume a is initially largest
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
@@ -21,6 +21,6 @@ public class Maximum {
 	// main method
 	public static void main(String[] args) {
 		// Calling method with Integer argument
-		Maximum.maxInteger(7, 10, 9);
+		Maximum.maxFloat(7.3f, 10.1f, 9.0f);
 	}
 }
