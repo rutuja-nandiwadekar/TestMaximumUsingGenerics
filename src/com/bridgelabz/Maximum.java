aed21edf31d3a9a566d35c5e560eccb156265f0a
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-24 21:50:08 +0530

    Added: Find the max of given 3 string values logic

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -2,18 +2,18 @@ package com.bridgelabz;
 
 public class Maximum {
 	/*
-	 * This method is used to Find maximum number among three Floats value
+	 * This method is used to Find maximum among three Strings value
 	 */
-	public static Float maxFloat(Float a, Float b, Float c) {
-		Float max = a;// assume a is initially largest
+	public static String maxString(String a, String b, String c) {
+		String max = a;// assume a is initially largest
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
 			max = c;
 
-		System.out.println("First Number= " + a);
-		System.out.println("Sec Number= " + b);
-		System.out.println("Thrird Number= " + c);
+		System.out.println("First String= " + a);
+		System.out.println("Sec String= " + b);
+		System.out.println("Third String= " + c);
 		System.out.println("Maximum Value is= " + max);
 		return max;
 	}
@@ -21,6 +21,6 @@ public class Maximum {
 	// main method
 	public static void main(String[] args) {
 		// Calling method with Integer argument
-		Maximum.maxFloat(7.3f, 10.1f, 9.0f);
+		Maximum.maxString("Apple", "Peach", "Banana");
 	}
 }
