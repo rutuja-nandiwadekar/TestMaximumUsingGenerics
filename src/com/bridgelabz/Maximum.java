e4d9eabfa84ef7f9af29a5570635301b1a5ab957
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-25 10:10:06 +0530

    Added: Method to take multiple parameters and find max value logic

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,6 +1,5 @@
 /*
- * Refactor to create Generic Class to take in 3
- * variables of Generic Type
+ * Extend the max method to take more then three parameters
  */
 package com.bridgelabz;
 
@@ -18,27 +17,29 @@ public class Maximum<T extends Comparable<T>> {
 	}
 
 	/*
-	 * This generic method is used to find max among 3 variables of Generic Type
+	 * This generic method is used to find max among more than 3 variables of
+	 * Generic Type
 	 */
-	public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
-		T max = a; // assume a is initially the largest
+	@SafeVarargs
+	public static <T extends Comparable<T>> T testMaximum(T... value) {
+		T max = value[0]; // assume a is initially the largest
 
-		if (b.compareTo(max) > 0) {
-			max = b;
+		for (int i = 1; i < value.length; i++) {
+			if (value[i].compareTo(max) > 0) {
+				max = value[i];
+			}
 		}
-		if (c.compareTo(max) > 0) {
-			max = c;
-		}
+		System.out.println("Maximum Value is= " + max);
 		return max; // returns the largest value
 	}
 
 	// main method
 	public static void main(String[] args) {
 		// Calling method And passing values
-		System.out.printf("Max of %d, %d and %d is %d\n\n", 3, 4, 5, Maximum.<Integer>testMaximum(3, 4, 5));
-		System.out.printf("Max of %.1f,%.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7,
-				Maximum.<Float>testMaximum(6.6f, 8.8f, 7.7f));
-		System.out.printf("Max of %s, %s and %s is %s\n", "Apple", "Pear", "Banana",
-				Maximum.<String>testMaximum("Apple", "Pear", "Banana"));
+		Maximum.<Integer>testMaximum(3, 4, 5, 9);
+
+		Maximum.<Float>testMaximum(6.6f, 8.8f, 7.7f, 9.3f);
+
+		Maximum.<String>testMaximum("Apple", "Pear", "Banana", "orange", "chiku");
 	}
 }
